52e98b9f28e1c0457f61930b7b3ce692e3d25cce
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2018-07-11 20:32:35 +0200

    Add test for Example Messages

---
+++ examples/src/test/java/de/unia/oc/robotcontrol/MockArduinoTest.java
@@ -0,0 +1,12 @@
+/* %FILE_TEMPLATE_TEXT% */
+package de.unia.oc.robotcontrol;
+
+import org.junit.jupiter.api.Test;
+
+public class MockArduinoTest {
+
+    @Test
+    void communicationWorks() {
+
+    }
+}
---
+++ examples/src/test/java/de/unia/oc/robotcontrol/message/ArduinoMessageTest.java
@@ -0,0 +1,47 @@
+/* %FILE_TEMPLATE_TEXT% */
+package de.unia.oc.robotcontrol.message;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ArduinoMessageTest {
+
+    @Test
+    void speedCommandMessageWorks() {
+        char direction = 's';
+        byte speed = (byte) (Math.random() * 255);
+
+        SpeedCmdMessage m = new SpeedCmdMessage(direction, speed);
+
+        Assertions.assertEquals(
+                ArduinoMessageTypes.SPEED_CMD.decode(m.getType().encode(m)),
+                m
+        );
+
+        Assertions.assertEquals(
+                ArduinoMessageTypes.ENCODING.decode(
+                        ArduinoMessageTypes.ENCODING.encode(m)),
+                m
+        );
+    }
+
+    @Test
+    void distanceDataMessageWorks() {
+        DistanceDataMessage m = new DistanceDataMessage(
+                (int) (Math.random() * 255),
+                (int) (Math.random() * 1023),
+                (int) (Math.random() * 2047)
+        );
+
+        Assertions.assertEquals(
+                ArduinoMessageTypes.DISTANCE_DATA.decode(m.getType().encode(m)),
+                m
+        );
+
+        Assertions.assertEquals(
+                ArduinoMessageTypes.DISTANCE_DATA.decode(
+                        ArduinoMessageTypes.DISTANCE_DATA.encode(m)),
+                m
+        );
+    }
+}
