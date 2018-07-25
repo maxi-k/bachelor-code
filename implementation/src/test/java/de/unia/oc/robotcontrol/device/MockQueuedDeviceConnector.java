b68f2249b3d5eaa2bd4f6d7c83711111469446a6
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2018-07-25 14:32:35 +0200

    Make Tests adhere to Nullness Typesystem from the checker framework

--- implementation/src/test/java/de/unia/oc/robotcontrol/device/MockQueuedDeviceConnector.java
+++ implementation/src/test/java/de/unia/oc/robotcontrol/device/MockQueuedDeviceConnector.java
@@ -5,7 +5,9 @@ import de.unia.oc.robotcontrol.coding.Encoding;
 import de.unia.oc.robotcontrol.concurrent.ScheduleProvider;
 import de.unia.oc.robotcontrol.flow.PassiveInFlow;
 import de.unia.oc.robotcontrol.message.Message;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
+import java.io.IOException;
 import java.util.function.Supplier;
 
 /**
@@ -13,7 +15,7 @@ import java.util.function.Supplier;
  */
 public class MockQueuedDeviceConnector extends QueuedDeviceConnector {
 
-    private byte[] mockMessage;
+    private byte @MonotonicNonNull [] mockMessage;
 
     public MockQueuedDeviceConnector(Encoding<Message> encoding,
                                      ScheduleProvider schedule,
@@ -28,7 +30,8 @@ public class MockQueuedDeviceConnector extends QueuedDeviceConnector {
     }
 
     @Override
-    protected byte[] retrieveMessage() {
+    protected byte[] retrieveMessage() throws IOException {
+        if (mockMessage == null) throw new IOException("No Message received that mock device could answer.");
         return mockMessage.clone();
     }
 
